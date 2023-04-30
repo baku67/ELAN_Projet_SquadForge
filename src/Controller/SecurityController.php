11d7d7b9c13e484e35679d3454c34b25daae9c2d
Basile(TUF) <basile08@hotmail.fr>
2023-04-30 16:10:00 +0200

    Init CSS + User + confirmationMail + Register

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+
+            if (!$this->getUser()->isVerified()) {
+                $_SESSION["error"] = "Your account is not verified.";
+                // throw new CustomUserMessageAuthenticationException('Your account is not verified.');
+            }
+            else {
+                return $this->redirectToRoute('app_home');
+            }
+        }
+
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/home', name: 'app_home')]
+    public function homepage()
+    {
+        return $this->render('security/home.html.twig', [
+            
+        ]);
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
