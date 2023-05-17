72d145378da59a81858b15eff8022efdeed278b0
baku67 <basile08@hotmail.fr>
2023-05-17 20:00:05 +0200

    Fix if(Role) + CensureType

--- src/Controller/ModerationController.php
+++ src/Controller/ModerationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Censure;
+use App\Form\CensureType;
 // use App\Entity\MediaPost;
 // use App\Entity\TopicPost;
 // use App\Entity\Game;
@@ -19,22 +20,55 @@ use Symfony\Component\HttpFoundation\Request;
 class ModerationController extends AbstractController
 {
     #[Route('/moderationDashboard', name: 'app_moderationDashboard')]
-    public function moderationDashboard(EntityManagerInterface $entityManager): Response
+    public function moderationDashboard(EntityManagerInterface $entityManager, Request $request): Response
     {
 
         if ( $this->getUser() && in_array('ROLE_MODO', $this->getUser()->getRoles()) ) {
+
             $censureRepo = $entityManager->getRepository(Censure::class);
-            $censureWords = $censureRepo->findAll();
+            $censureWords = $censureRepo->findBy([], ["creation_date" => "DESC"]);
     
+            // Ajouter le formType d'ajout
+            $censure = new Censure;
+            $form = $this->createForm(CensureType::class, $censure);
+            $form -> handleRequest($request);
+
+            if($form->isSubmitted()) {
+
+                if($form->isValid()) {
+
+                    $censure = $form->getData();
+    
+                    // Init de la publish_date du comment
+                    $censure->setCreationDate(new \DateTime());
+                    $censure->setUser($this->getUser());
+
+                    // Maj BDD
+                    $entityManager->persist($censure);
+                    $entityManager->flush();
+
+                    $this->addFlash('success', 'Le mot a été ajouté');
+                    return $this->redirectToRoute('app_moderationDashboard');
+
+                }
+
+            }
+
+
             return $this->render('moderation/index.html.twig', [
+                'formAddCensoredWord' => $form->createView(),
                 'censureWords' => $censureWords
             ]);
     
         }
+        else {
 
-        return $this->render('moderation/index.html.twig', [
-            
-        ]);
+            $this->addFlash('error', 'Vous devez être connecté ou modérateur pour accéder à cette page');
+            return $this->redirectToRoute('app_login');
+
+        }
+
+        
 
     }
 }
---
+++ src/Form/CensureType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\Censure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CensureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('word', TextType::class, [
+                // 'label' => 'Message',
+                'required' => true,
+                'attr' => [
+                    "class" => "form-control",
+                    'placeholder' => 'Votre message...'
+                    // 'rows' => 1
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                // 'label' => 'Publier',
+                'label' => '<i class="fa-solid fa-paper-plane"></i>',
+                'label_html' => true,
+                'attr' => ["class" => "btn btn-primary"]
+            ]);
+            // ->add('creation_date')
+            // ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Censure::class,
+        ]);
+    }
+}
