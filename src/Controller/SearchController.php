27ae0a485c3558dada299198509b93e4b19cbea4
baku67 <basile08@hotmail.fr>
2023-07-31 22:43:17 +0200

    WIP landingPage + filtres

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    #[Route('/searchLandingPage/{textInput}', name: 'app_searchLandingPage')]
+    public function index(): Response
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+}
